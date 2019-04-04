e98bc562654bcc1786afda9e246ee13637ad622b
Tamerlan2002 <46812872+Tamerlan2002@users.noreply.github.com>
2019-04-04 11:35:01 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var ex1 = document.getElementById('mydiv')
+    ex1.style.animation= 'mymove'
+    ex1.style.animationDuration = '0.5s';
+    ex1.style.webkitAnimationDelay = '0s'
+    ex1.style.animationFillMode = "both;"
+    ex1.style.animationIterationCount = '2'
+}
